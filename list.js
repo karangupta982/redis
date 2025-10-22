5e079335cd41f34a9e6d0f845186f569d2cdc583
Karan gupta <gupta.karan1.gh@gmail.com>
2025-10-22 16:57:31 +0530

    list

---
+++ list.js
@@ -0,0 +1,25 @@
+const client = require('./client');
+
+async function listinit(){
+    await client.del('fruits');
+
+    await client.lpush('fruits', 'apple');
+    await client.lpush('fruits', 'banana');
+    await client.rpush('fruits', 'kiwi')
+
+    let items = await client.lrange('fruits',0,1);
+    console.log("Fruits:", items)
+    items = await client.lrange('fruits',0,-1); // get all items
+    console.log("Fruits:", items)
+    const popped = await client.lpop('fruits');
+    console.log("Popper itemL", popped);
+    items = await client.lrange('fruits',0,-1);
+
+    await client.ltrim('fruits',0,1)
+    items = await client.lrange('fruits',0,-1);
+    console.log("Fruits after ltrim:", items);
+
+
+}
+
+listinit();
