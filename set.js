e312e3cb9c8d441dc87ec439ef95c66d352c69d8
Karan gupta <gupta.karan1.gh@gmail.com>
2025-10-23 16:22:36 +0530

    set

---
+++ set.js
@@ -0,0 +1,28 @@
+const client = require('./client');
+
+async function setinit(){
+    await client.del('students');
+    await client.sadd('students','Tanish', 'Sharad', 'Priyanshu');
+    let members = await client.smembers('students');
+    console.log("members: ", members);
+
+    await client.sadd('students', 'loki');
+    const afterRemove = await client.srem('students','loki');
+    console.log('after srem', afterRemove);
+    const size = await client.scard('students');
+    console.log("size",size);
+
+    // const finalMembers = await client.spop('students')
+    // console.log("final members", finalMembers);
+
+    members = await client.smembers('students');
+    console.log("members: ", members);
+    isExist = await client.sismember('students','Tanish');
+    console.log("isExist:", isExist);
+
+    members = await client.srandmember('students');
+    console.log("random member:", members);
+    
+}
+
+setinit();
