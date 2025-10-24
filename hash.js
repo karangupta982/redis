d0467b54833ff1f96ce802e951a80fd3f6ca38a2
Karan gupta <gupta.karan1.gh@gmail.com>
2025-10-24 10:31:15 +0530

    hash

--- hash.js
+++ hash.js
@@ -11,16 +11,25 @@ async function hashinit(){
                 city: 'Delhi'
             }
         );
-        console.log(res);
+        // console.log(res);
         
-        await client.hset('user:1','Profession','Upcoming IAS');
+        await client.hset('user:1','profession','Upcoming IAS');
         const user = await client.hgetall('user:1');
-        console.log('user:',user);
+        // console.log('user:',user);
         await client.hset('user:1','age',26);
         const updatedUser = await client.hgetall('user:1');
-        console.log('updated User:', updatedUser);
+        // console.log('updated User:', updatedUser);
 
-        
+        const res1 = await client.hget('user:1','profession');
+        // console.log("prefession:",res1);
+
+        const res2 = await client.hmget('user:1',['name','age','profession']);
+        console.log(res2);
+
+        const res3 = await client.hincrby('user:1','age',10);
+        console.log("res3:", res3);
+
+
     }
     catch(err){
         console.log("error: ",err);
